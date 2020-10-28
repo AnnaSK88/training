01ebacaa2c1d8ba9e99d0d7b1fe93265e9fef876
Anna SK <anna.sapun89@gmail.com>
2020-10-28 13:16:32 +0100

    Update file structure

---
+++ basics/function-arguments.js
@@ -0,0 +1,9 @@
+// function math(one, two, three) {
+//     return (two * three) + one
+// }
+// console.log(math(53, 61, 67))//4140
+
+
+const argument = [53, 61, 67]
+const newExample = argument[1] * argument[2] + argument[0]
+console.log(newExample) //4140
