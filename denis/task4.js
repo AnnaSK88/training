c2559e2bd4cd18a62879bd317438c5cc19ff0854
Anna SK <anna.sapun89@gmail.com>
2020-10-31 18:54:52 +0100

    My comments

--- denis/task4.js
+++ denis/task4.js
@@ -4,29 +4,41 @@ const closer = ']}"'
 
 const isCorrect = (brackets) => {
 
-const o = [];
-const c = [];
-let broken;
-const arr = brackets.split('');
+    const o = [];
+    const c = [];
+    let broken;
+    const arr = brackets.split('');
 
-for (let i = 0; i < arr.length; i++) {
-    const item = arr[i];
-    const index = opener.indexOf(item);
-    console.log(index);
-    if (index != -1) {
-        o.push(item);
-        c.push(closer[index]);
-        continue;   
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        const index = opener.indexOf(item);
+        console.log(index);
+        if (index != -1) {
+            o.push(item);
+            c.push(closer[index]);
+            continue;
+        }
+        if (c.pop() !== item) {
+            broken = true;
+            break;
+        }
+        o.pop();
     }
-    if (c.pop() !== item) {
-        broken = true;
-        break;
-    }
-    o.pop();
-}
 
-if (o.length || broken ) return false;
-return true;
+    if (o.length || broken) return false;
+    return true;
 }
 
 console.log(isCorrect(brackets));
+
+//является ли строка корректной 
+//в корректной строке каждой открывающей скобке должна соответствовать парная закрывающая
+
+//1 создаем переменные строки, которые пок-т начало и конец строки
+
+//2. ф-ция, проверяющая корректность строки:
+//  2.1вводим переменные и присваиваем им значене массива,
+//   имеющуюся строку также типиз-м в массив
+
+//  2.2 для введеных данных вставляем условия  преобразования
+//- пройдя цикл в консоль будет выведен индекс начала массива преобразованной строки
--- denis/task9.js
+++ denis/task9.js
@@ -1,7 +1,6 @@
 // Для произвольного теста нам нужно посчитать статистику, нужно вывести количество символов в тексте с пробелами, без пробелов, количество слов в тексте, посчитать среднюю длину слова.
 const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum sodales vestibulum. Fusce iaculis purus vel mauris suscipit dapibus. Aliquam ultrices sed justo non tincidunt. In lacus ligula, ullamcorper in sem in, venenatis euismod elit. In lacinia ex non eleifend vehicula. Sed vel magna enim. Vivamus tortor lorem, tempor eu venenatis a, rutrum a nulla. Phasellus vitae quam interdum.'
 
-
 const cleanText = (text) => {
     let clearText = text.replace(/\./g, '');
     return clearText.replace(/\,/g, '');
@@ -12,9 +11,10 @@ const lenWithoutSpaces = (text) => text.replace(/ /g, '').length;
 const countWords = (text) => text.split(' ').length;
 const countAwerageWordLen = (text) => {
     const words = text.split(' ');
-    return words.reduce((len, word) => len+=word.length, 0) / words.length;
+    return words.reduce((len, word) => len += word.length, 0) / words.length;
 }
-const cleanText = cleanText(text);
+const clearText = cleanText(text);
+
 console.log('length with spaces')
 console.log(lenWithSpaces(clearText))
 console.log('length without spaces')
@@ -23,3 +23,29 @@ console.log('words')
 console.log(countWords(clearText))
 console.log('word average len')
 console.log(countAwerageWordLen(clearText))
+
+// консоль не работает
+// выдает ошибку Identifier 'cleanText' has already been declared
+
+
+//1. переменная типа строка с произвольным текст
+//2. вводим переменную cleanText, к-ая отраб-т ф-цию:
+//   2.1 заменяет все точки и запятые на пустые строки
+
+//3. вводим переменную, определяющая длину текста с пробелами
+//   const lenWithSpaces = (text) => text.length;
+
+//4. вводим переменную , опр длину текста БЕЗ пробелов
+//   заданная ф-ция убирает пробелы, соединяя воедино текст 
+//   const lenWithoutSpaces = (text) => text.replace(/ /g, '').length;
+
+//5. вводим переменную с количеством слов, для этого
+//  5.1 создаем ф-цию, к-ая преобразует текст в массив
+//       и считаем кол-во элементов в нем
+//   const countWords = (text) => text.split(' ').length;
+
+// 6. расчет средней длины слова
+//  6.1 создаем ф-цию, к-ая преобразует исходный текст
+//     - перем-ной слова присваиваем значение теста, преоб-ого в массив
+//     - возвращаем ф-ции значение, которое получено
+//       вычислением сумма длины массива деленная на длину слова
